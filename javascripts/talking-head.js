67a9ec2ced3fcdb33545a21f3c89890290a06bc5
Erik Froese <froesecom@gmail.com>
2016-06-04 14:19:52 +1000

    set up structure for dynamic resize of sprite

--- javascripts/talking-head.js
+++ javascripts/talking-head.js
@@ -1,8 +1,10 @@
 var erik = {
   sprite: {
     frame: {
-      width: 345,
-      height: 588
+      baseWidth: 345,
+      baseHeight: 588,
+      width: 172.5,
+      height: 294
     },
     animate: false,
     moveToFrame(n) {
@@ -13,6 +15,14 @@ var erik = {
     talkingFrames: [1,2,1,0]
   }
 };
+
+erik.init = function(){
+  //dynamically set height and width here
+  //use baseWidth/baseHeight to set width/height
+  $("#erik-head").height(erik.sprite.frame.height)
+  $("#sprite-erik").height(erik.sprite.frame.height).width(erik.sprite.frame.width).show();
+
+}
 
 erik.talk = function(){
   erik.sprite.animate = true;
