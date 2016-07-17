76675d2b8205cee7c19cc30a511fae234dac5007
Erik <erik@hhd.com.au>
2016-07-17 11:42:06 +1000

    add content for idle sayings

--- javascripts/content.js
+++ javascripts/content.js
@@ -1,5 +1,7 @@
 CONTENT = {
   idleSayings: [
-    "This website is a work in progress, obvs."
+    "This website is a work in progress, obvs. ",
+    "I can't wait until the fireworks arrive. Come back in a week or two. ",
+    "Not impressed? I'm not finished yet! "
   ]
 }
