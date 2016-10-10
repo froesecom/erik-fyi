919822b94c42c47ded60fd31177f3bb86ab6b9c8
Erik <erik@hhd.com.au>
2016-10-10 13:02:06 +1100

    fade in email address after page load to avoid scraping

--- javascripts/application.js
+++ javascripts/application.js
@@ -4,6 +4,13 @@ $(function(){
     doomMachine.fireItUp();
   });
 
+  //build email address so shady bots don't scrape it
+  setTimeout(function(){
+    e_array = ["its", "@", "erik.fyi"];
+    $(".email").html("<a href='mailto:" + e_array.join("") + "'>" + e_array.join("" ) + "</a>").fadeIn();
+  }, 800);
+
+  //event listeners
   $('.menu-btn').click(function(){
     $('.menu').toggleClass('expand');
     $(this).toggleClass('menu-btn-open');
